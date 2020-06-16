d588ce64dec55d940deab3d9d7e916e282a24e69
Fabio Mattei <burattino@gmail.com>
2020-06-16 15:40:15 +0200

    addin example about how to set text in an entry widget

---
+++ basics/2020-06-16-entry/entry2.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+'''
+This example demonstrates the Gtk::Entry widget.
+It demostrates how it is possible to set the text contained in an entry.
+
+Author: Fabio Mattei
+Website: https://fabiomattei.github.io/rubygtkfun
+Last modified: June 2020
+'''
+
+
+require 'gtk3'
+
+app = Gtk::Application.new("org.rubygtkfun.entry", :flags_none)
+
+def on_key_release sender, event
+  puts sender.text
+end
+
+app.signal_connect "activate" do |application|
+  window = Gtk::ApplicationWindow.new application
+  window.set_title "My first window with a label"
+  window.set_default_size 400, 400
+
+  entry = Gtk::Entry.new
+  window.add entry
+  entry.text= 'Hello World, I am an entry'
+
+  window.show_all
+end
+
+puts app.run
